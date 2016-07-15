d4427d03805c42cc818095c662d60473553609a8
hwangtiger <hwangtiger@gmail.com>
2016-07-15 14:05:52 -0500

    add diff_function

--- basic_manipulation.R
+++ basic_manipulation.R
@@ -18,6 +18,8 @@ VCF_file_dir <- "../Analysis/"
 vcf_file_D <- paste0(VCF_file_dir, "n_197_D_annotated.vcf.gz")
 vcf_D <- read.vcfR(vcf_file_D, verbose = FALSE)
 
+donor_table <- as.data.frame(vcf_D@fix, stringsAsFactors = FALSE)
+
 D_chr <- as.data.frame(vcf_D@fix[vcf_D@fix[,1] == Chrom, ], stringsAsFactors = FALSE)
 D_chr$POS <- as.integer(D_chr$POS)
 
@@ -38,3 +40,45 @@ geneInfo <- GRCh38_gene_list[which(GRCh38_gene_list$GeneName == GeneName)[1], ]
 
 D_stats <- gene_variant_stats(geneInfo, D_chr)
 R_stats <- gene_variant_stats(geneInfo, R_chr)
+
+# length(unique(donor_table$CHROM))
+
+dd <- cbind(D_stats$all_variants[which(D_stats$all_variants$POS %in% R_stats$all_variants$POS), 1:5], 
+            R_stats$all_variants[which(R_stats$all_variants$POS %in% D_stats$all_variants$POS), 1:5])
+
+D_same_variants <- D_stats$all_variants[which(D_stats$all_variants$POS %in% R_stats$all_variants$POS), ]
+R_same_variants <- R_stats$all_variants[which(R_stats$all_variants$POS %in% D_stats$all_variants$POS), ]
+
+D_diff_variants <- D_stats$all_variants[-which(D_stats$all_variants$POS %in% R_stats$all_variants$POS), ]
+R_diff_variants <- R_stats$all_variants[-which(R_stats$all_variants$POS %in% D_stats$all_variants$POS), ]
+
+
+#######
+# Check all the genes in the list
+#######
+GRCh38_gene_list_chr <- GRCh38_gene_list[which(GRCh38_gene_list$chrom == Chrom), ]
+gene_names <- unique(GRCh38_gene_list_chr$GeneName)
+num_genes <- length(gene_names)
+
+diff_num <- vector(mode = "integer", length = num_genes)
+
+for (id in 1:num_genes){
+  
+  GeneName <- gene_names[id]
+  geneInfo <- GRCh38_gene_list_chr[which(GRCh38_gene_list_chr$GeneName == GeneName)[1], ]
+  
+  D_stats <- gene_variant_stats(geneInfo, D_chr)
+  R_stats <- gene_variant_stats(geneInfo, R_chr)
+
+  D_same_variants <- D_stats$all_variants[which(D_stats$all_variants$POS %in% R_stats$all_variants$POS), ]
+  R_same_variants <- R_stats$all_variants[which(R_stats$all_variants$POS %in% D_stats$all_variants$POS), ]
+  
+#   if(!identical(D_same_variants$ALT, R_same_variants$ALT)){
+#     
+#   }
+  
+  D_diff_variants <- D_stats$all_variants[-which(D_stats$all_variants$POS %in% R_stats$all_variants$POS), ]
+  R_diff_variants <- R_stats$all_variants[-which(R_stats$all_variants$POS %in% D_stats$all_variants$POS), ]
+  
+  diff_num[id] <- dim(D_diff_variants)[1] + dim(R_diff_variants)[1]
+}
