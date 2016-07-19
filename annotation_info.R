713612c0a4acfdb8a00611dad2748d80c887dcb3
hwangtiger <hwangtiger@gmail.com>
2016-07-18 22:47:30 -0500

    Add Annotation Parsing function

--- annotation_info.R
+++ annotation_info.R
@@ -12,9 +12,25 @@ vcf_D <- read.vcfR(vcf_file_D, verbose = FALSE)
 D_chr_variants <- as.data.frame(vcf_D@fix[vcf_D@fix[,1] == Chrom, ], stringsAsFactors = FALSE)
 D_chr_variants$POS <- as.integer(D_chr_variants$POS)
 
-D_chr_meta <- vcf_D@meta
 D_chr_gt <- as.data.frame(vcf_D@gt[vcf_D@fix[,1] == Chrom, ], stringsAsFactors = FALSE)
 D_chr_gt <- D_chr_gt[(D_chr_variants$FILTER == "PASS"), ]
 D_chr_variants <- D_chr_variants[(D_chr_variants$FILTER == "PASS"), ] # only look at high-quality variants
 
-# rm(vcf_D)
+D_chr_meta <- vcf_D@meta
+
+num_meta <- length(D_chr_meta)
+format_id <- which(grepl("FORMAT=", D_chr_meta))
+info_id <- which(grepl("INFO=", D_chr_meta))
+
+# Format
+strsplit(gsub("##FORMAT=", "",D_chr_meta[format_id]), ",")
+
+# Annotation
+#D_chr_meta[info_id]
+ANN_id <- which(grepl("ANN", D_chr_meta[info_id]))
+ann_colnames <- gsub(" ", "", unlist(strsplit(unlist(strsplit(D_chr_meta[info_id[ANN_id]],"'"))[2], "\\|")))
+
+aa <- sapply(1:length(D_chr_variants$INFO), 
+             function(x) unlist(strsplit(unlist(strsplit(D_chr_variants$INFO[x], split = "ANN="))[2], "\\|")))
+ab <- as.data.frame(aa)
+#
--- util.R
+++ util.R
@@ -51,3 +51,122 @@ RegionNumber2RegionName <- function(RegionNumber, num_reigons){
   }
   return(regionName)
 }
+
+###############
+# Extract SnpEff Annotation from VCF file
+###############
+extract_annotation <- function(annotation, Chrom_meta_info, Chrom_variants_info){
+  
+  if(is.na(Chrom_variants_info) || is.na(Chrom_meta_info)) stop("Empty info!")
+  annotation_id <- which(grepl(annotation, Chrom_meta_info))
+  ann_colnames <- gsub(" ", "", unlist(strsplit(unlist(strsplit(Chrom_meta_info[annotation_id],"'"))[2], "\\|")))
+  ann_colnames <- c("PredictionID", ann_colnames)
+  num_columns <- length(ann_colnames)
+  
+  num_rows <- length(Chrom_variants_info)
+  
+  Chrom_ANN_info <- as.data.frame(matrix(data = "", nrow = num_rows, ncol = num_columns), stringsAsFactors = FALSE)
+  colnames(Chrom_ANN_info) <- ann_colnames
+  
+#  for(id in 1:num_rows){
+    all_info <- unlist(strsplit(Chrom_variants_info, ";"))
+    all_info <- gsub("\\(", "", all_info)
+    all_info <- gsub("\\)", "", all_info)
+    ann_index <- which(grepl(annotation, all_info))
+    
+    allele_info <- unlist(strsplit(all_info[ann_index], ","))
+    
+    if(length(allele_info) == 1){ # if there are only one allele info
+      ann_info <- c(1, unlist(strsplit(unlist(strsplit(all_info[ann_index], split = paste0(annotation,"=")))[2], "\\|")))
+      if(length(ann_info) < num_columns) {
+        ann_info <- c(ann_info, rep("", num_columns - length(ann_info)))
+      }
+      names(ann_info) <- ann_colnames
+    }else{ # if there are more than one allele info
+      ann_info <- c(1, unlist(strsplit(unlist(strsplit(allele_info[1], split = paste0(annotation,"=")))[2], "\\|")))
+      ann_info <- rbind(ann_info, t(sapply(2:length(allele_info), function(x) c(x, unlist(strsplit(allele_info[x], "\\|"))))))
+      if(dim(ann_info)[2] < num_columns) {
+        ann_info <- cbind(ann_info, rep("", num_columns -dim(ann_info)[2]))
+      }
+      colnames(ann_info) <- ann_colnames
+    }
+    # Chrom_ANN_info[id, ] <- ann_info
+    Chrom_ANN_info <- ann_info
+#  }
+  return(Chrom_ANN_info)
+}
+
+##################
+# Parse Info from VCF files
+##################
+parse_meta_info <- function(Chrom_meta_info, Chrom_variants_info){
+  
+  num_rows <- length(Chrom_variants_info)
+  num_info_ID <- length(Chrom_meta_info)
+  
+  INFO_colnames <- sapply(1:(num_info_ID -3), function(x) unlist(strsplit(unlist(strsplit(Chrom_meta_info[x], ","))[1],"="))[3])
+    
+  original_INFO <- as.data.frame(matrix(data = NA, nrow = num_rows, ncol = num_info_ID - 3))
+  colnames(original_INFO) <- INFO_colnames
+  
+  ANNotation_info <- NULL
+  LOF_info <- NULL
+  NMD_info <- NULL
+  
+  for(id in 1:num_rows){
+    all_info <- unlist(strsplit(Chrom_variants_info[id], ";"))
+    ann_index <-which(grepl("ANN=", all_info))
+    
+    if(ann_index > 1){ # if there is original info, in addition to ANNotation
+      
+      for(jd in 1:(ann_index-1)){
+        temp_info <- unlist(strsplit(all_info[jd], "="))
+        original_INFO[id, which(INFO_colnames %in% temp_info[1])] <- temp_info[2]
+      }
+      
+      temp_info <- extract_annotation("ANN", Chrom_meta_info, Chrom_variants_info[id])
+      ANNotation_info <- rbind(ANNotation_info, cbind(index = rep(id, dim(temp_info)[1]), temp_info))
+
+      if(ann_index < length(all_info)){ # if there is LOF information
+        temp_info <- cbind(id, extract_annotation("LOF", Chrom_meta_info, Chrom_variants_info[id]))
+        LOF_info <- rbind(LOF_info, cbind(index = rep(id, dim(temp_info)[1]), temp_info))
+      }
+      if(length(all_info) - ann_index == 2){ # if there is NMD information
+        temp_info <- cbind(id, extract_annotation("NMD", Chrom_meta_info, Chrom_variants_info[id]))
+        NMD_info <- rbind(NMD_info, cbind(index = rep(id, dim(temp_info)[1]), temp_info))
+      }
+      
+    }else if(ann_index == 1){ # if there is no original info
+      
+      temp_info <- extract_annotation("ANN", Chrom_meta_info, Chrom_variants_info[id])
+      ANNotation_info <- rbind(ANNotation_info, cbind(index = rep(id, dim(temp_info)[1]), temp_info))
+      
+      if(ann_index < length(all_info)){ # if there is LOF information
+        temp_info <- cbind(id, extract_annotation("LOF", Chrom_meta_info, Chrom_variants_info[id]))
+        LOF_info <- rbind(LOF_info, cbind(index = rep(id, dim(temp_info)[1]), temp_info))
+      }
+      if(length(all_info) - ann_index == 2){ # if there is NMD information
+        temp_info <- cbind(id, extract_annotation("NMD", Chrom_meta_info, Chrom_variants_info[id]))
+        NMD_info <- rbind(NMD_info, cbind(index = rep(id, dim(temp_info)[1]), temp_info))
+      }
+    } #else{ # if there's no ANNotation info
+      
+   # }
+    
+  }
+  return(list(original_INFO = original_INFO,
+              Annotation_INFO = ANNotation_info,
+              LossOfFunction_IFO = LOF_info,
+              NonsenseMedDecay_INFO = NMD_info))
+}
+#############
+# Visualization
+####
+# plot.genome <- function(){
+#   require(ggplot2)
+#   
+#   p <- ggplot(diff_num, aes(factor(cyl), mpg))
+#   
+#   p + geom_boxplot()
+#   
+# }
