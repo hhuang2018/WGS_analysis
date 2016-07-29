ebb85119600c8e872cf8e82e9c3f70c7d788626b
hwangtiger <hwangtiger@gmail.com>
2016-07-29 10:51:20 -0500

    debug: run as system commands

--- vcf_bgzip_tabix.R
+++ vcf_bgzip_tabix.R
@@ -1,4 +1,4 @@
-vcf_file_dir <- "/mnt/cloudbiodata_nfs_1/hli_scratch/hhuang/hli_vcf_preprocessed/"
+vcf_file_dir <- "/mnt/cloudbiodata_nfs_1/hli_scratch/hhuang/hli_vcf_annotated_preprocessed/"
 
 all_vcf_files <- list.files(vcf_file_dir, pattern = "\\.vcf.gz$")
 
@@ -7,9 +7,9 @@ num_files <- length(all_vcf_files)
 for(id in 1:num_files){
   
   vcf_name <- gsub(".gz", "", all_vcf_files[id])
-  eval(parse(text = paste0("gunzip ", vcf_file_dir, all_vcf_files[id])))
-  eval(parse(text = paste0("bgzip ", vcf_file_dir, vcf_name)))
-  eval(parse(text = paste0("tabix -p vcf ", vcf_file_dir, all_vcf_files[id])))
+  system(paste0("cd ", vcf_file_dir,"; gunzip ", all_vcf_files[id]))
+  system(paste0("cd ", vcf_file_dir,"; bgzip ", vcf_file_dir, vcf_name))
+  system(paste0("cd ", vcf_file_dir,"; tabix -p vcf ", vcf_file_dir, all_vcf_files[id]))
   
   cat(all_vcf_files[id], " done! \n")
   
