eb0a68d9092c52eb09fa6d31957206df90182301
hwangtiger <hwangtiger@gmail.com>
2016-10-13 09:58:11 -0500

    change of file name

--- paired_vcf_stats_summary.R
+++ paired_vcf_stats_summary.R
@@ -1,10 +1,11 @@
+
 library(vcfR)
 source('util.R', echo = FALSE)
 
 vcf_file_dir <- "/mnt/cloudbiodata_nfs_1/hli_scratch/hhuang/paired_vcf/"
 #vcf_file_dir <- "../paired_vcf/"
 all_vcf_files <- list.files(vcf_file_dir, pattern = "\\.vcf.gz$")
-output_dir <- "../Output/SKAT_format/"
+output_dir <- "/mnt/cloudbiodata_nfs_1/hli_scratch/hhuang/paired_vcf_Stats_Summary/"
 
 num_files <- length(all_vcf_files)
 
