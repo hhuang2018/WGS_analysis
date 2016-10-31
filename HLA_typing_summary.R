3d128137a074f5c85d85260e457c39754d43a56c
hwangtiger <hwangtiger@gmail.com>
2016-10-30 20:50:41 -0500

    General summary of HLA typings

--- HLA_typing_summary.R
+++ HLA_typing_summary.R
@@ -136,3 +136,315 @@ rownames(donor_sex) <- c("donorMale", "donorFemale")
 chisq.test(donor_sex, correct = T)  # p = 0.2011
 fisher.test(donor_sex) # p = 0.1673
 
+#########################################
+## summary of ambiguous typings
+#########################################
+source("util.R")
+
+library(ggplot2)
+# library(xlsx)
+# HLA_typings <- read.xlsx2(file = "../HLI_hla_mg_v3.xlsx", sheetIndex = 1)
+HLA_typings <- read.csv(file = "../HLI_hla_mg_v3.csv")
+
+# avialable IDs
+load("../Data/ID_table.RData")
+
+R_aa <- sort(c(as.character(HLA_typings$r_a_typ1), as.character(HLA_typings$r_a_typ2)))
+D_aa <- sort(c(as.character(HLA_typings$d_a_typ1), as.character(HLA_typings$d_a_typ2)))
+
+num_pairs <- dim(HLA_typings)[1]
+unmatched <- list()
+HLA_typing <- data.frame(ID = numeric(dim(HLA_typings)[1]*6),
+                         typ1 = character(dim(HLA_typings)[1]*6),
+                         typ2 = character(dim(HLA_typings)[1]*6), 
+                         Ambiguous = numeric(dim(HLA_typings)[1]*6),
+                         Group = character(dim(HLA_typings)[1]*6),
+                         stringsAsFactors = F)
+
+available_IDs <-ID_table[ID_table$GroupID %in% ID_table$GroupID[duplicated(ID_table$GroupID)],]
+
+available_ID_index <- which(HLA_typings$nmdp_rid %in% available_IDs$R_D_ID)
+
+for(id in 1:num_pairs){
+  
+  HLA_typing$ID[(6*(id-1)+1):(6*id)] <- id
+  
+  if(id %in% available_ID_index){
+    groupType <- available_IDs$Group[available_IDs$R_D_ID %in% HLA_typings$nmdp_rid[id]]
+    if(groupType == "n") HLA_typing$Group[(6*(id-1)+1):(6*id)] <- "non-aGVHD" else if(groupType == "a") HLA_typing$Group[(6*(id-1)+1):(6*id)] <- "aGVHD"
+  }
+  
+  ### HLA-A
+  HLA_A_d <- sort(c(as.character(HLA_typings$d_a_typ1[id]), as.character(HLA_typings$d_a_typ2[id])))
+  HLA_A_r <- sort(c(as.character(HLA_typings$r_a_typ1[id]), as.character(HLA_typings$r_a_typ2[id])))
+  if(identical(HLA_A_d, HLA_A_r)){ # matching
+    HLA_typing[6*(id-1)+1, c(2,3)] <- HLA_A_d
+    AA <- strsplit(HLA_A_d, "\\*")
+    HLA_typing$Ambiguous[6*(id-1)+1] <-length(which(sapply(AA, function(x) grepl("[[:alpha:]]", x[2]))))
+  }else { # not matching
+    if(HLA_A_d[1] != HLA_A_r[1]){ # First allele, if not matching
+      # remove false positive mismatches
+      HLA_A_d_field <- unlist(strsplit(HLA_A_d[1], ":"))
+      HLA_A_r_field <- unlist(strsplit(HLA_A_r[1], ":"))
+      if(HLA_A_d_field[1] == HLA_A_r_field[1] & HLA_A_d_field[2] == HLA_A_r_field[2]){
+        HLA_typing$typ1[6*(id-1)+1] <-  paste0(HLA_A_d[1], "~" ,HLA_A_r[1]) # false positive
+      }else HLA_typing$typ1[6*(id-1)+1] <- paste0(HLA_A_d[1], "<>" ,HLA_A_r[1]) 
+      
+    }else{ # if the first allele is matching
+      HLA_typing$typ1[6*(id-1)+1] <- HLA_A_d[1]
+    }
+    
+    if(HLA_A_d[2] != HLA_A_r[2]){ # second allele
+      # remove false positive mismatches
+      HLA_A_d_field <- unlist(strsplit(HLA_A_d[2], ":"))
+      HLA_A_r_field <- unlist(strsplit(HLA_A_r[2], ":"))
+      if(HLA_A_d_field[1] == HLA_A_r_field[1] & HLA_A_d_field[2] == HLA_A_r_field[2]){
+        HLA_typing$typ2[6*(id-1)+1] <-  paste0(HLA_A_d[2], "~" ,HLA_A_r[2]) # false positive
+      }else HLA_typing$typ2[6*(id-1)+1] <- paste0(HLA_A_d[2], "<>" ,HLA_A_r[2])
+  
+    }else{# if the second allele is matching
+      HLA_typing$typ2[6*(id-1)+1] <- HLA_A_d[2]
+    }
+    
+    AA <- strsplit(unique(c(HLA_A_d, HLA_A_r)), "\\*")
+    HLA_typing$Ambiguous[6*(id-1)+1] <-length(which(sapply(AA, function(x) grepl("[[:alpha:]]", x[2]))))
+  }
+  
+  ### HLA-B
+  HLA_B_d <- sort(c(as.character(HLA_typings$d_b_typ1[id]), as.character(HLA_typings$d_b_typ2[id])))
+  HLA_B_r <- sort(c(as.character(HLA_typings$r_b_typ1[id]), as.character(HLA_typings$r_b_typ2[id])))
+  if(identical(HLA_B_d, HLA_B_r)){
+    HLA_typing[6*(id-1)+2, c(2,3)] <- HLA_B_d
+    BB <- strsplit(HLA_B_d, "\\*")
+    HLA_typing$Ambiguous[6*(id-1)+2] <-length(which(sapply(BB, function(x) grepl("[[:alpha:]]", x[2]))))
+  }else{
+    # HLA_typing[6*(id-1)+2, c(2,3)] <- "No Matching"
+    if(HLA_B_d[1] != HLA_B_r[1]){
+      # remove false positive mismatches
+      HLA_B_d_field <- unlist(strsplit(HLA_B_d[1], ":"))
+      HLA_B_r_field <- unlist(strsplit(HLA_B_r[1], ":"))
+      if(HLA_B_d_field[1] == HLA_B_r_field[1] & HLA_B_d_field[2] == HLA_B_r_field[2]){
+        HLA_typing$typ1[6*(id-1)+2] <-  paste0(HLA_B_d[1], "~" ,HLA_B_r[1]) # false positive
+      }else HLA_typing$typ1[6*(id-1)+2] <- paste0(HLA_B_d[1], "<>" ,HLA_B_r[1])
+      
+    }else{
+      HLA_typing$typ1[6*(id-1)+2] <- HLA_B_d[1]
+    }
+    if(HLA_B_d[2] != HLA_B_r[2]){ # second allele
+      # remove false positive mismatches
+      HLA_B_d_field <- unlist(strsplit(HLA_B_d[2], ":"))
+      HLA_B_r_field <- unlist(strsplit(HLA_B_r[2], ":"))
+      if(HLA_B_d_field[1] == HLA_B_r_field[1] & HLA_B_d_field[2] == HLA_B_r_field[2]){
+        HLA_typing$typ2[6*(id-1)+2] <-  paste0(HLA_B_d[2], "~" ,HLA_B_r[2]) # false positive
+      }else HLA_typing$typ2[6*(id-1)+2] <- paste0(HLA_B_d[2], "<>" ,HLA_B_r[2])
+      
+    }else{
+      HLA_typing$typ2[6*(id-1)+2] <- HLA_B_d[2]
+    }
+
+    BB <- strsplit(unique(c(HLA_B_d, HLA_B_r)), "\\*")
+    HLA_typing$Ambiguous[6*(id-1)+2] <-length(which(sapply(BB, function(x) grepl("[[:alpha:]]", x[2]))))
+  }
+  
+  ## HLA-C
+  HLA_C_d <- sort(c(as.character(HLA_typings$d_c_typ1[id]), as.character(HLA_typings$d_c_typ2[id])))
+  HLA_C_r <- sort(c(as.character(HLA_typings$r_c_typ1[id]), as.character(HLA_typings$r_c_typ2[id])))
+  if(identical(HLA_C_d, HLA_C_r)){
+    HLA_typing[6*(id-1)+3, c(2,3)] <- HLA_C_d
+    CC <- strsplit(HLA_C_d, "\\*")
+    HLA_typing$Ambiguous[6*(id-1)+3] <- length(which(sapply(CC, function(x) grepl("[[:alpha:]]", x[2]))))
+  }else{
+    # HLA_typing[6*(id-1)+3, c(2,3)] <- "No Matching"
+    if(HLA_C_d[1] != HLA_C_r[1]){
+      # remove false positive mismatches
+      HLA_C_d_field <- unlist(strsplit(HLA_C_d[1], ":"))
+      HLA_C_r_field <- unlist(strsplit(HLA_C_r[1], ":"))
+      if(HLA_C_d_field[1] == HLA_C_r_field[1] & HLA_C_d_field[2] == HLA_C_r_field[2]){
+        HLA_typing$typ1[6*(id-1)+3] <-  paste0(HLA_C_d[1], "~" ,HLA_C_r[1]) # false positive
+      }else HLA_typing$typ1[6*(id-1)+3] <- paste0(HLA_C_d[1], "<>" ,HLA_C_r[1])
+      
+      
+    }else{
+      HLA_typing$typ1[6*(id-1)+3] <- HLA_C_d[1]
+    }
+    
+    if(HLA_C_d[2] != HLA_C_r[2]){
+      # remove false positive mismatches
+      HLA_C_d_field <- unlist(strsplit(HLA_C_d[2], ":"))
+      HLA_C_r_field <- unlist(strsplit(HLA_C_r[2], ":"))
+      if(HLA_C_d_field[1] == HLA_C_r_field[1] & HLA_C_d_field[2] == HLA_C_r_field[2]){
+        HLA_typing$typ2[6*(id-1)+3] <-  paste0(HLA_C_d[2], "~" ,HLA_C_r[2]) # false positive
+      }else HLA_typing$typ2[6*(id-1)+3] <- paste0(HLA_C_d[2], "<>" ,HLA_C_r[2])
+      
+     
+    }else{
+      HLA_typing$typ2[6*(id-1)+3] <- HLA_C_d[2]
+    }
+    
+    
+    CC <- strsplit(unique(c(HLA_C_d, HLA_C_r)), "\\*")
+    HLA_typing$Ambiguous[6*(id-1)+3] <-length(which(sapply(CC, function(x) grepl("[[:alpha:]]", x[2]))))
+  }
+  
+  ##### HLA-DRB1
+  HLA_DRB1_d <- sort(c(as.character(HLA_typings$d_drb1_typ1[id]), as.character(HLA_typings$d_drb1_typ2[id])))
+  HLA_DRB1_r <- sort(c(as.character(HLA_typings$r_drb1_typ1[id]), as.character(HLA_typings$r_drb1_typ2[id])))
+  if(identical(HLA_DRB1_d, HLA_DRB1_r)){
+    HLA_typing[6*(id-1)+4, c(2,3)] <- HLA_DRB1_d
+    DRBB <- strsplit(HLA_DRB1_d, "\\*")
+    HLA_typing$Ambiguous[6*(id-1)+4] <- length(which(sapply(DRBB, function(x) grepl("[[:alpha:]]", x[2]))))
+  }else{
+    # HLA_typing[6*(id-1)+4, c(2,3)] <- "No Matching"
+    
+    if(HLA_DRB1_d[1] != HLA_DRB1_r[1]){
+      # remove false positive mismatches
+      HLA_DRB1_d_field <- unlist(strsplit(HLA_DRB1_d[1], ":"))
+      HLA_DRB1_r_field <- unlist(strsplit(HLA_DRB1_r[1], ":"))
+      if(HLA_DRB1_d_field[1] == HLA_DRB1_r_field[1] & HLA_DRB1_d_field[2] == HLA_DRB1_r_field[2]){
+        HLA_typing$typ1[6*(id-1)+4] <-  paste0(HLA_DRB1_d[1], "~" ,HLA_DRB1_r[1]) # false positive
+      }else HLA_typing$typ1[6*(id-1)+4] <- paste0(HLA_DRB1_d[1], "<>" ,HLA_DRB1_r[1])
+      
+    }else{
+      HLA_typing$typ1[6*(id-1)+4] <- HLA_DRB1_d[1]
+    }
+    if(HLA_DRB1_d[2] != HLA_DRB1_r[2]){
+      # remove false positive mismatches
+      HLA_DRB1_d_field <- unlist(strsplit(HLA_DRB1_d[2], ":"))
+      HLA_DRB1_r_field <- unlist(strsplit(HLA_DRB1_r[2], ":"))
+      if(HLA_DRB1_d_field[1] == HLA_DRB1_r_field[1] & HLA_DRB1_d_field[2] == HLA_DRB1_r_field[2]){
+        HLA_typing$typ2[6*(id-1)+4] <-  paste0(HLA_DRB1_d[2], "~" ,HLA_DRB1_r[2]) # false positive
+      }else HLA_typing$typ2[6*(id-1)+4] <- paste0(HLA_DRB1_d[2], "<>" ,HLA_DRB1_r[2])
+      
+      
+    }else{
+      HLA_typing$typ2[6*(id-1)+4] <- HLA_DRB1_d[2]
+    }
+    
+    
+    DRBB <- strsplit(unique(c(HLA_DRB1_d, HLA_DRB1_r)), "\\*")
+    HLA_typing$Ambiguous[6*(id-1)+4] <-length(which(sapply(DRBB, function(x) grepl("[[:alpha:]]", x[2]))))
+  }
+  
+  ####### HLA-DQB1
+  HLA_DQB1_d <- sort(c(as.character(HLA_typings$d_dqb1_typ1[id]), as.character(HLA_typings$d_dqb1_typ2[id])))
+  HLA_DQB1_r <- sort(c(as.character(HLA_typings$r_dqb1_typ1[id]), as.character(HLA_typings$r_dqb1_typ2[id])))
+  if(identical(HLA_DQB1_d, HLA_DQB1_r)){
+    HLA_typing[6*(id-1)+5, c(2,3)] <- HLA_DQB1_d
+    DQBB <- strsplit(HLA_DQB1_d, "\\*")
+    HLA_typing$Ambiguous[6*(id-1)+5] <- length(which(sapply(DQBB, function(x) grepl("[[:alpha:]]", x[2]))))
+  }else{
+    # HLA_typing[6*(id-1)+5, c(2,3)] <- "No Matching"
+    
+    if(HLA_DQB1_d[1] != HLA_DQB1_r[1]){ # first allele
+      # remove false positive mismatches
+      HLA_DQB1_d_field <- unlist(strsplit(HLA_DQB1_d[1], ":"))
+      HLA_DQB1_r_field <- unlist(strsplit(HLA_DQB1_r[1], ":"))
+      if(HLA_DQB1_d_field[1] == HLA_DQB1_r_field[1] & HLA_DQB1_d_field[2] == HLA_DQB1_r_field[2]){
+        HLA_typing$typ1[6*(id-1)+5] <-  paste0(HLA_DQB1_d[1], "~" ,HLA_DQB1_r[1]) # false positive
+      }else HLA_typing$typ1[6*(id-1)+5] <- paste0(HLA_DQB1_d[1], "<>" ,HLA_DQB1_r[1])
+      
+      
+    }else{
+      HLA_typing$typ1[6*(id-1)+5] <- HLA_DQB1_d[1]
+    }
+    if(HLA_DQB1_d[2] != HLA_DQB1_r[2]){
+      # remove false positive mismatches
+      HLA_DQB1_d_field <- unlist(strsplit(HLA_DQB1_d[2], ":"))
+      HLA_DQB1_r_field <- unlist(strsplit(HLA_DQB1_r[2], ":"))
+      if(HLA_DQB1_d_field[1] == HLA_DQB1_r_field[1] & HLA_DQB1_d_field[2] == HLA_DQB1_r_field[2]){
+        HLA_typing$typ2[6*(id-1)+5] <-  paste0(HLA_DQB1_d[2], "~" ,HLA_DQB1_r[2]) # false positive
+      }else HLA_typing$typ2[6*(id-1)+5] <- paste0(HLA_DQB1_d[2], "<>" ,HLA_DQB1_r[2])
+      
+      
+    }else{
+      HLA_typing$typ2[6*(id-1)+5] <- HLA_DQB1_d[2]
+    }
+    
+    DQBB <- strsplit(unique(c(HLA_DQB1_d, HLA_DQB1_r)), "\\*")
+    HLA_typing$Ambiguous[6*(id-1)+5] <-length(which(sapply(DQBB, function(x) grepl("[[:alpha:]]", x[2]))))
+  }
+  
+  ##### HLA-DPB1
+  HLA_DPB1_d <- sort(c(as.character(HLA_typings$d_dpb1_typ1[id]), as.character(HLA_typings$d_dpb1_typ2[id])))
+  HLA_DPB1_r <- sort(c(as.character(HLA_typings$r_dpb1_typ1[id]), as.character(HLA_typings$r_dpb1_typ2[id])))
+  if(identical(HLA_DPB1_d, HLA_DPB1_r)){
+    HLA_typing[6*id, c(2,3)] <- HLA_DPB1_d
+    DPBB <- strsplit(HLA_DPB1_d, "\\*")
+    HLA_typing$Ambiguous[6*id] <- length(which(sapply(DPBB, function(x) grepl("[[:alpha:]]", x[2]))))
+  }else{
+    # HLA_typing[6*id, c(2,3)] <- "No Matching"
+    
+    if(HLA_DPB1_d[1] != HLA_DPB1_r[1]){
+      # remove false positive mismatches
+      HLA_DPB1_d_field <- unlist(strsplit(HLA_DPB1_d[1], ":"))
+      HLA_DPB1_r_field <- unlist(strsplit(HLA_DPB1_r[1], ":"))
+      
+      if(length(HLA_DPB1_r_field) >0 & length(HLA_DPB1_d_field) >0 
+         & HLA_DPB1_d_field[1] == HLA_DPB1_r_field[1] & HLA_DPB1_d_field[2] == HLA_DPB1_r_field[2]){
+        HLA_typing$typ1[6*id] <-  paste0(HLA_DPB1_d[1], "~" ,HLA_DPB1_r[1]) # false positive
+      }else HLA_typing$typ1[6*id] <- paste0(HLA_DPB1_d[1], "<>" ,HLA_DPB1_r[1])
+      
+     
+    }else{
+      HLA_typing$typ1[6*id] <- HLA_DPB1_d[1]
+    }
+    if(HLA_DPB1_d[2] != HLA_DPB1_r[2]){
+      # remove false positive mismatches
+      HLA_DPB1_d_field <- unlist(strsplit(HLA_DPB1_d[2], ":"))
+      HLA_DPB1_r_field <- unlist(strsplit(HLA_DPB1_r[2], ":"))
+      if(length(HLA_DPB1_r_field) >0 & length(HLA_DPB1_d_field) >0 
+         & HLA_DPB1_d_field[1] == HLA_DPB1_r_field[1] & HLA_DPB1_d_field[2] == HLA_DPB1_r_field[2]){
+        HLA_typing$typ2[6*id] <-  paste0(HLA_DPB1_d[2], "~" ,HLA_DPB1_r[2]) # false positive
+      }else HLA_typing$typ2[6*id] <- paste0(HLA_DPB1_d[2], "<>" ,HLA_DPB1_r[2])
+      
+      
+    }else{
+      HLA_typing$typ2[6*id] <- HLA_DPB1_d[2]
+    }
+    
+    DPBB <- strsplit(unique(c(HLA_DPB1_d, HLA_DPB1_r)), "\\*")
+    HLA_typing$Ambiguous[6*id] <-length(which(sapply(DPBB, function(x) grepl("[[:alpha:]]", x[2]))))
+  }
+}
+
+HLA_typing_stats_ten <- HLA_typing[-6*(1:num_pairs), ]
+sum(HLA_typing_stats_ten$Ambiguous) ### 676 typings are ambiguous out of 1255 ~  0.5386454 
+sum(HLA_typing$Ambiguous)  ### 1039 typings are ambiguous out of 1506 ~ 0.689907  (6 loci)
+
+length(which(HLA_typing$Group!=""))/6  # 205 groups have WGS data
+
+available_HLA_typing <- HLA_typing[HLA_typing$Group!="", ]
+available_HLA_typing_ten <- available_HLA_typing[-6*(1:205), ]
+sum(available_HLA_typing$Ambiguous)  ### 912 typings are ambiguous, out of 1230 ~ 0.7414634 (6 loci)
+sum(available_HLA_typing_ten$Ambiguous)  ### 606 typings are ambiguous, out 1025 ~ 0.5912195
+
+available_IDs <-ID_table[ID_table$GroupID %in% ID_table$GroupID[duplicated(ID_table$GroupID)],]
+
+availble_IDs_HLA_typing <- HLA_typings[(HLA_typings$nmdp_rid %in% available_IDs$R_D_ID),]
+
+Ambiguous_groups <- which(sapply(1:205, function(x) sum(available_HLA_typing_ten$Ambiguous[(5*(x-1)+1):(5*x)]))==0)
+##  70 groups out of 205 groups ambiguous typings 
+
+table(available_HLA_typing_ten$Group[Ambiguous_groups])
+# #    aGVHD non-aGVHD 
+# #      28        42 
+
+# No matching
+typ1_mismatch <- which( grepl("<>",available_HLA_typing_ten$typ1))
+typ2_mismatch <- which( grepl("<>",available_HLA_typing_ten$typ2))
+length(unique(c(typ1_mismatch, typ2_mismatch)))  ## 85
+table(available_HLA_typing_ten$Group[unique(c(typ1_mismatch, typ2_mismatch))])
+# aGVHD non-aGVHD 
+# 40        45 
+# remove false positive
+
+## different resolution
+typ1_res <- which( grepl("~",available_HLA_typing_ten$typ1))
+typ2_res <- which( grepl("~",available_HLA_typing_ten$typ2))
+length(unique(c(typ1_res, typ2_res)))  ## 57
+table(available_HLA_typing_ten$Group[unique(c(typ1_res, typ2_res))])
+# aGVHD non-aGVHD 
+# 28        29 
+# remove false positive
+
+length(unique(c(typ1_mismatch, typ2_mismatch, typ1_res, typ2_res)))
