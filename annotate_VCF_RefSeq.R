8bfa48d56d01f180592f4b4f2c042389264044e1
hwangtiger <hwangtiger@gmail.com>
2017-02-28 22:54:56 -0600

    Update

--- annotate_VCF_RefSeq.R
+++ annotate_VCF_RefSeq.R
@@ -14,26 +14,27 @@ Not_annotated_index <- which(!(filenames %in% annotated_filenames))
 num_files <- length(Not_annotated_index)
 
 for(id in 1:num_files){
-  t1 <- system(paste0("java -Xmx16g -jar ", snpEff_dir, "snpEff.jar -v GRCh38.82 ",
-                      original_file_dir, filenames[Not_annotated_index[id]], ".vcf.gz > ",
-                      destination_dir, filenames[Not_annotated_index[id]],"_annotated.vcf"),
+  t1 <- system(paste0("java -Xmx16g -jar ", snpEff_dir, "snpEff.jar -noStats -v GRCh38.p7.RefSeq ", 
+                      # destination_dir, filenames[Not_annotated_index[id]], "_snpEff_RefSeq_summary.csv -v ",
+                       original_file_dir, filenames[Not_annotated_index[id]], ".vcf.gz > ",
+                      destination_dir, filenames[Not_annotated_index[id]],"_RefSeq_annotated.vcf"),
                intern = TRUE)
-  system(paste0("bgzip ", destination_dir, filenames[Not_annotated_index[id]],"_annotated.vcf"))
-  system(paste0("tabix -p vcf ", destination_dir, filenames[Not_annotated_index[id]],"_annotated.vcf.gz"))
-  system(paste0("mv snpEff_genes.txt ",  destination_dir, filenames[Not_annotated_index[id]], "_snpEff_genes.txt"))
-  system(paste0("mv snpEff_summary.html ",  destination_dir, filenames[Not_annotated_index[id]], "_snpEff_summary.html"))
-  save(t1, file = paste0(destination_dir, filenames[Not_annotated_index[id]],"_annotated.out.RData"))
+  system(paste0("bgzip ", destination_dir, filenames[Not_annotated_index[id]],"_RefSeq_annotated.vcf"))
+  system(paste0("tabix -p vcf ", destination_dir, filenames[Not_annotated_index[id]],"_RefSeq_annotated.vcf.gz"))
+  system(paste0("mv snpEff_genes.txt ",  destination_dir, filenames[Not_annotated_index[id]], "_snpEff_RefSeq_genes.txt"))
+  system(paste0("mv snpEff_summary.html ",  destination_dir, filenames[Not_annotated_index[id]], "_snpEff_RefSeq_summary.html"))
+  save(t1, file = paste0(destination_dir, filenames[Not_annotated_index[id]],"_RefSeq_annotated.out.RData"))
 }
 
 ##### need csv format of summary
-for(id in 1:num_files){
-  system(paste0("java -Xmx16g -jar ", snpEff_dir, "snpEff.jar ann -csvStats ",
-                      destination_dir, filenames[Not_annotated_index[id]], "_snpEff_summary.csv -v ",
-                      "GRCh38.82 ", original_file_dir, filenames[Not_annotated_index[id]], ".vcf.gz > ", 
-                      destination_dir, filenames[Not_annotated_index[id]],"_annotated.vcf"), 
-               intern = TRUE)
-  system(paste0("rm ", destination_dir, filenames[Not_annotated_index[id]],"_annotated.vcf"))
-  system(paste0("rm ", destination_dir, filenames[Not_annotated_index[id]], "_snpEff_summary.genes.txt"))
-  # system(paste0("mv snpEff_summary.csv ",  destination_dir, filenames[Not_annotated_index[id]], "_snpEff_summary.csv"))
-  #save(t1, file = paste0(destination_dir, filenames[Not_annotated_index[id]],"_annotated.out.RData"))
-}
+# for(id in 1:num_files){
+#   system(paste0("java -Xmx16g -jar ", snpEff_dir, "snpEff.jar ann -csvStats ",
+#                       destination_dir, filenames[Not_annotated_index[id]], "_snpEff_summary.csv -v ",
+#                       "GRCh38.82 ", original_file_dir, filenames[Not_annotated_index[id]], ".vcf.gz > ", 
+#                       destination_dir, filenames[Not_annotated_index[id]],"_annotated.vcf"), 
+#                intern = TRUE)
+#   system(paste0("rm ", destination_dir, filenames[Not_annotated_index[id]],"_annotated.vcf"))
+#   system(paste0("rm ", destination_dir, filenames[Not_annotated_index[id]], "_snpEff_summary.genes.txt"))
+#   # system(paste0("mv snpEff_summary.csv ",  destination_dir, filenames[Not_annotated_index[id]], "_snpEff_summary.csv"))
+#   #save(t1, file = paste0(destination_dir, filenames[Not_annotated_index[id]],"_annotated.out.RData"))
+# }
