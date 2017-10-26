86e02e3fa967322ceb55980dae409047cdaac09a
hwangtiger <hwangtiger@gmail.com>
2017-10-26 14:36:48 -0500

    debug2

--- Missense_mismatch_DR_pair_summary.R
+++ Missense_mismatch_DR_pair_summary.R
@@ -9,22 +9,11 @@ output_dir <- "/mnt/cloudbiodata_nfs_2/users/hhuang/vcf_missense_variants_wwVers
 
 all_vcf_files <- list.files(VCF_file_dir, pattern = "\\.txt$")
 
-file_names <- all_vcf_files[grepl("_R_", all_vcf_files)] # 216 donors; 240 recipients
+#file_names <- all_vcf_files[grepl("_R_", all_vcf_files)] # 216 donors; 240 recipients
 
-# AML patients only
-load("../Data/HLI_available_pairs_dis_table.RData")
-
-AML_ids <- 1:length(Available_paired_table$Disease)#which(Available_paired_table$Disease %in% "AML") # 196
-RID <- sapply(1:length(file_names), function(x) as.numeric(unlist(strsplit(file_names[x], "_"))[4]))
-
-AML_RID_ids <- which(RID %in% Available_paired_table$RID[AML_ids])
-file_names <- file_names[AML_RID_ids]
-#####
-num_files <- length(file_names)  
-missense_mm_stats <- vector(mode = "integer", length = num_files)
 for(id in 1:num_files){
   
-  vcf_file <- paste0(VCF_file_dir, file_names[id])
+  vcf_file <- paste0(VCF_file_dir, all_vcf_files[id])
   # vcf_info <- read.vcfR(vcf_file, verbose = FALSE)
   # temp_info <- vcf_info@fix
   # vcf_missense <- temp_info[temp_info[,"FILTER"] == "PASS", c("CHROM", "POS", "REF","ALT")]
